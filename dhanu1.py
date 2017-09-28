a6f35325c489c083f9067722e9213315ee3d1649
DHANUSH49 <31614844+DHANUSH49@users.noreply.github.com>
2017-09-28 08:53:31 +0530

    Update dhanu1.py

--- dhanu1.py
+++ dhanu1.py
@@ -1,7 +1,7 @@
 import smtplib
 s = smtplib.SMTP('smtp.dhanushdhanu5427@gmail.com', 587)
 s.starttls()
-s.login("dhanushdhanu5427@gmail.com", "9019055031")
+s.login("dhanushdhanu5427@gmail.com", "*********8")
 message = "hi wat r u doing"
 s.sendmail("dhanushdhanu5427@gmail.com", "dhanushdhanu5427@gmail.com", message)
 s.quit()
