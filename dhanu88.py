a177ed0a6e85f99305df357a410499b15c73f1e4
DHANUSH49 <31614844+DHANUSH49@users.noreply.github.com>
2017-09-28 09:11:57 +0530

    Update dhanu88.py

--- dhanu88.py
+++ dhanu88.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 import random
+# system choses random numbers
 n=(int(input("press 7 to play")))
 i=0
 j=0
 while(n==7):
     t=["Rock","Paper","Scissors"]
     Computer=t[random.randint(0,2)]
+    # system chooses a random number from 0 to 2
     print("Enter your Choice:-")
+    # it is used to choose your choice
     Player="False"
     Player=input("Rock, Paper, Scissors")
     if(Computer==Player):
         print("Tie!")
     elif(Computer=="Rock"):
+        # system chooses its choice 
         if(Player=="Paper"):
             print("The computer played: ",Computer)
             print("The Player played: ",Player)
@@ -41,6 +45,7 @@ while(n==7):
             i=i+1
         else:
             print("Computer Wins")
+            # system wins
             j=j+1
     else:
         print("Enter Choice again")
